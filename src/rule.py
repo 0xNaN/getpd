75b2928c786ef683d13911ffc8a01df05aba353d
NaN <enzodicicco@gmail.com>
2014-02-04 19:00:30 +0100

    Refactoring _sliceFromRule

--- src/rule.py
+++ src/rule.py
@@ -1,97 +1,96 @@
 import re
 
-"""
-A class to handle Rule as pair of RegEX and Interval
-"""
 class Rule:
+    """ A class to handle Rule as pair of RegEX and Slice object. """
     _regex = ""
     _slice = None
 
-    def __init__(self, strrule):
-        """ Build Rule Object """
-
-        if not self._checkPattern(strrule):
+    def __init__(self, rule):
+        if not self._checkPattern(rule):
             raise ValueError('Malformed Rule')
 
-        self._regex = self._regexFromRule(strrule)
-        self._slice = self._sliceFromRule(strrule)
+        self._regex = self._regexFromRule(rule)
+        self._slice = self._sliceFromRule(rule)
 
         if not self._isRegexCorrect(self._regex):
             raise ValueError('Malformed RegEX')
 
-
-    def _regexFromRule(self, strrule):
-        """ Get the RegEX inside a rule """
-
-        rule = strrule[1:-1].rsplit('][')
-
+    def _regexFromRule(self, rule):
+        """ Get the RegEX inside a rule. """
+        rule = rule[1:-1].rsplit('][')
         return rule[0]
 
-    def _sliceFromRule(self, strrule):
-        """ Build a slice object from a Rule
-            NOTE: In according to slice class, if a slice
-            is build only with a value it is the 'stop' value.
+    def _sliceFromRule(self, rule):
+        """ Build a slice object from a Rule.
+
+            N.B: In according to slice class, if a slice
+            is build only with an args it is the 'stop' value.
         """
 
         start = stop = step = None
 
         # split Slice from Rule, obataining: x:x:x
-        slicedata = strrule[1:-1].rsplit('][')
-        slicedata = slicedata[1]
+        slice_data = rule[1:-1].rsplit('][')
+        slice_data = slice_data[1]
 
-        # find start, stop and step
-        slicedata = slicedata.split(':')
-        numtoken = len(slicedata)
+        # split slice_data to find start, stop and step
+        slice_data = slice_data.split(':')
 
-        if numtoken > 3:
-            raise ValueError('Malformed Slice: too args')
+        # convert slice_data to int
+        slice_data = self._stringIndexToInt(slice_data)
 
-        # Converting in Int, '' must be consider 0
-        for i in range(0, numtoken):
-            try:
-                slicedata[i] = int(slicedata[i])
-            except:
-                if slicedata[i] == '':
-                    slicedata[i] = 0
-                else:
-                    raise ValueError('Malformed Slice: unallowed char')
+        # assign index to right var
+        ntoken = len(slice_data)
 
-        if numtoken == 1:
+        if ntoken == 1:
             # if only an arg given, it is 'stop'
-            # with or without colon symbol
-            stop = slicedata[0]
-
-        elif numtoken == 2:
-            start = slicedata[0]
-            stop = slicedata[1]
-
-        elif numtoken == 3:
-            start = slicedata[0]
-            stop = slicedata[1]
-            step = slicedata[2]
-
-            # N.B: step shouldn't be 0! if omitted is 1 or None
+            # with, or without, colon symbol
+            stop = slice_data[0]
+        elif ntoken == 2:
+            # start and stop, [:]
+            start = slice_data[0]
+            stop = slice_data[1]
+        elif ntoken == 3:
+            # start, stop and step [::]
+            start = slice_data[0]
+            stop = slice_data[1]
+            step = slice_data[2]
+            # step must != 0
             if step == 0: step = 1
+        elif ntoken > 3:
+            raise ValueError('Malformed Slice: too args')
 
         return slice(start, stop, step)
 
-
-
-    def _checkPattern (self, strrule):
-        """ Check if a Rule has a correct form """
-
-        match = re.match('^\[.*\]\[[0-9:+-]*\]$', strrule)
+    def _checkPattern (self, rule):
+        """ Check if a Rule has the correct form as [][x:x:x] """
+        match = re.match('^\[.*\]\[[0-9:+-]*\]$', rule)
 
         if match:
             return True
         return False
 
-    def _isRegexCorrect (self, regex):
+    def _isRegexCorrect(self, regex):
+        """ Check if a regex is compilable. """
         correct = True
-
         try:
             re.compile(regex)
         except:
             correct = False
-
         return correct
+
+    def _stringIndexToInt(self, indexes):
+        """ Cast a list of string index to a list of int index.
+
+            N.B: If an index is equal to '' it is considered 0.
+        """
+        for i in range(0, len(indexes)):
+            try:
+                if indexes[i] == '':
+                    indexes[i] = 0
+                else:
+                    indexes[i] = int(indexes[i])
+            except:
+                raise ValueError('Malformed Slice: check value')
+        return indexes
+
